bd869f1cead51c001785c8bff7081820d43de9ba
Joseph <gmremcorat@gmail.com>
2020-02-09 16:50:44 -0500

    Initial Commit

---
+++ SwiftUITesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITesting
+//
+//  Created by Joseph Storer on 2/9/20.
+//  Copyright © 2020 Joseph Storer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
